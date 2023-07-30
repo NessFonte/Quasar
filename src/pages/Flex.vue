5200407ebe5a55bea7c44850511faf36c2c63a24
neisser <neisser2909@gmail.com>
2023-07-30 16:24:09 -0400

    Implementado Flexbox en Flex.vue

--- src/pages/Flex.vue
+++ src/pages/Flex.vue
@@ -32,7 +32,63 @@
     <div class="row q-mb-md justify-between">
       <div class="col-12 col-sm-6 col-md-4 bg-primary"></div>
       <div class="col-12 col-sm-6 col-md-4 bg-negative"></div>
-    </div>    
+    </div> 
+    
+    <q-separator spaced />
+
+    <div class="row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="row q-mb-md justify-between">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="row q-mb-md justify-end">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="row q-mb-md justify-around">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="column q-mb-md items-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="column q-mb-md items-end bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-between bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-positive"></div>
+      <div class="square-box bg-negative"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-between bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box self-end bg-positive"></div>
+      <div class="square-box self-start bg-negative"></div>
+    </div>
   </q-page>
 </template>
 
@@ -47,5 +103,14 @@ export default defineComponent({
 <style scoped>
 .col, .col-4, .col-6, .col-12 {
   height: 50px;
+}
+
+.square-box {
+  width: 50px;
+  height: 50px;
+}
+
+.custom-height {
+  height: 300px;
 }
 </style>
