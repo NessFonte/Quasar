b67dfd3278af369dca6d06733f1348a0e897b1e8
neisser <neisser2909@gmail.com>
2023-07-30 06:47:23 -0400

    Utilizadas algunas tipografías de Quasar

--- src/pages/Typography.vue
+++ src/pages/Typography.vue
@@ -1,6 +1,28 @@
 <template>
-  <q-page class="flex">
-    <h1>Typography.vue</h1>
+  <q-page class="flex column q-pa-md">
+    <span class="text-h1">Typography 1</span>
+    <span class="text-h2">Typography 2</span>
+    <span class="text-h3">Typography 3</span>
+    <span class="text-h4">Typography 4</span>
+    <span class="text-h5">Typography 5</span>
+    <span class="text-h6">Typography 6</span>
+    
+    <span class="text-subtitle1">Subtitle 1</span>
+    <span class="text-subtitle2">Subtitle 1</span>
+    
+    <q-separator />
+    <p class="text-body1">Et culpa laboris cupidatat in quis id. Culpa amet aliqua reprehenderit magna voluptate ut nulla nulla. Proident sunt eiusmod elit veniam adipisicing quis in adipisicing irure magna cupidatat.</p>
+    <p class="text-body2">Exercitation dolore labore deserunt ea nostrud laboris officia qui proident ipsum. Minim reprehenderit velit ullamco et cupidatat non. Nulla sit excepteur fugiat voluptate proident irure dolor. Lorem quis voluptate Lorem est aute irure. In enim in minim consequat mollit. Amet ad culpa elit excepteur sit voluptate.</p>
+    <q-separator />
+    
+
+    <span class="text-weight-thin">Ullamco exercitation ex ipsum consectetur commodo ullamco anim non.</span>
+    <span class="text-weight-light">Adipisicing dolore duis ipsum labore exercitation Lorem.</span>
+    <span class="text-weight-regular">Ipsum commodo pariatur adipisicing non qui anim fugiat do voluptate.</span>
+    <span class="text-weight-medium">Dolor exercitation reprehenderit quis consectetur eiusmod aliquip adipisicing quis laboris id duis culpa voluptate do.</span>
+    <span class="text-weight-bold">Excepteur irure culpa incididunt sit.</span>
+    <span class="text-weight-bolder">Voluptate laboris et eu officia magna nulla cupidatat veniam commodo quis et occaecat.</span>
+ 
   </q-page>
 </template>
 
